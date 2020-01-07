2bbd38369f3813d030b49830f99dd233c4a6e7e2
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-07 00:33:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,6 +35,6 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   base = monopoly_with_third_tier
-  #base[:railroads][:names][:reading_railroad] = $"100"
+  base[:railroads][:names][:reading_railroad] = {"mortgage_value" => $"100"}
   base
 end
