20d131a4f75e37627f9744ff7cffdf31beddd91a
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-07 00:31:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,5 +34,6 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  
+  base = monopoly_with_third_tier
+  base[:railroads][:names][]
 end
