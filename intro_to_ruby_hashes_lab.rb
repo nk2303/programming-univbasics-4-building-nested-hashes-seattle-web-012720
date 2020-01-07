89fb7c5d0b51ec051ecb5b2258e991e131d71f5a
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-07 00:20:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200}}
-  base[:railroads][:name] = "somehting"
+  base[:railroads][:names] = "somehting"
   base
 end
 
