2c23e1e79d5bafcfd1072b543eb0c288ed63a075
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-07 00:36:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,5 +36,6 @@ def monopoly_with_fourth_tier
   # complexity.
   base = monopoly_with_third_tier
   base[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+  base[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
   base
 end
