065b49d128fb53a12b11c96ffbb9900372bfa7eb
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-07 00:03:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,10 @@ def monopoly_with_third_tier
   # complexity.
   #monopoly_with_second_tier
   base = monopoly_with_second_tier
-  base[:railroads] = { :rent_in_dollars => {}}
+  base[:railroads] = { :rent_in_dollars => {:one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200}}
   base
 end
 
